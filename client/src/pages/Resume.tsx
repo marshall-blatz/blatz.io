dcc74bc1bf968bb21e6d1d965264cb85d96635e6
Marshall Blatz <marshallpblatz@gmail.com>
2023-08-26 00:36:36 -0500

    fixed dependency issue

--- client/src/pages/Resume.tsx
+++ client/src/pages/Resume.tsx
@@ -9,7 +9,7 @@ export default function Resume({}: Props) {
         <h1>/resume</h1>
         <p>Here is my resume as of August 2023.</p>
       </div>
-      <iframe src="../assets/resume.pdf" style={{ width:"800px", height:"1100px"}}/>
+      <iframe src="/assets/resume.pdf" style={{ width:"800px", height:"1100px"}}/>
     </Layout>
   )
 }
